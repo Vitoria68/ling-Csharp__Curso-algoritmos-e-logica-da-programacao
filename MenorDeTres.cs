4058ddfecda401c2d01d7ebaa29d521dad6d5960
Vitória 3 <96185574+Vitoria68@users.noreply.github.com>
2024-05-14 15:57:10 -0300

    Add files via upload

---
+++ MenorDeTres.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenorDeTres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, menor;
+
+            Console.Write("Primeiro valor: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Segundo valor: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Terceiro valor: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a < b && a < c)
+            {
+                menor = a;
+            }
+            else if (b < c)
+            {
+                menor = b;
+            }
+            else
+            {
+                menor = c;
+            }
+
+            Console.WriteLine("MENOR = " + menor);
+        }
+    }
+}
